ad6600684b3fa82988deb748d3d65312ae374cf1
Anujesh Ansh <anshanujesh@gmail.com>
2024-05-18 17:14:01 +0530

    Changes

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. Here I constantly update about my latest projects.
+    
+    <h4>Updates:-</h4>
+    -> Portfolio
+    </br>
+    -> Resume
+        `,    
+    "cs-degree": `Behold, the future home of my coveted CS degree! 🎓 Soon, it will grace this wall with its presence, a symbol of my unwavering dedication and love for all things tech! 💻✨`,
+    "sofa-table": `Welcome to my relaxation heaven! 🛋️ Here lies the throne of tranquility, where I indulge in hours of lounging and unwinding. And hey, if you're a music enthusiast, dive into <a href="https://youtube.com/playlist?list=PLA7GXXzlrPK8Eech7Q_f0CrMVl09uxVnv&si=2vP1qJXX33CULnof" target="_blank"><b>my eclectic YouTube playlist</b></a>! It's a musical odyssey spanning across languages and genres, from K-pop to Bollywood, J-pop to English hits, and beyond! 🎶🎮🎨`,
+    tv: `Enter my entertainment realm, where Shinchan reigns supreme! 📺 Join me for animated adventures aplenty! And for those seeking worldly wisdom, I've got just the ticket: <a href="https://www.youtube.com/@IcyTalesvideos" target="_blank"><b>IcyTales</b></a>, an awe-inspiring YouTube channel that blends entertainment with enlightenment! 🚀🤖`,
+    bed: `Behold, my chamber of creativity and contemplation! 💡 This cozy corner is where brilliance is born, especially during those midnight musings. When inspiration strikes, it's a race against time to capture those fleeting ideas before they vanish into the night! ✍️💭`,
+    resume: `Step into my professional domain, where my resume stands as a testament to my skills and aspirations! 💼 Curious about my journey? Take a peek at <a href="https://anujesh-ansh.github.io/Anujesh_Ansh-Resume/" target="_blank"><b>My Resume</b></a> and drop me a line at anshanujesh@gmail.com if you've got an exciting opportunity knocking at your door! 📝🚀`,
+    hobbies: `Embark on a journey through my world of passions, where each hobby adds a unique flavor to life's tapestry! 🎸🍳🎨 From strumming melodies on the guitar to whipping up culinary wonders in the kitchen, every moment is an adventure! And let's not forget my love for ethical hacking and tinkering with IoT gadgets—it's a whirlwind of excitement and exploration! 🌟`,
+    repository: `<b>Welcome to my treasure trove of intriguing and captivating projects! 🌟</b>
+    </br>Explore them on my <a href="https://github.com/Anujesh-Ansh?tab=repositories" target="blank">GitHub Repo</a> where innovation meets creativity! 
+    Among them, a few are still in progress, but fear not, they'll sparkle with updates soon! ✨🚀`,
+    exit: `Do u Wanna Leave ?? 
+    </br></br>
+    Just close the tab.`,
+};
